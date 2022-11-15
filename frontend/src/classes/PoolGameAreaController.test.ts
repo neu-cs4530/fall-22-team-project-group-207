625d7468a197455ff81b04bbaa391b8574be06fa
jmcmanusx47 <jamesmcmanusx47@gmail.com>
2022-11-15 02:50:52 -0500

    last 1 for sure

--- frontend/src/classes/PoolGameAreaController.test.ts
+++ frontend/src/classes/PoolGameAreaController.test.ts
@@ -1,4 +1,4 @@
-import { mock, mockClear/*, MockProxy*/ } from 'jest-mock-extended';
+import { mock, mockClear /*, MockProxy*/ } from 'jest-mock-extended';
 import { nanoid } from 'nanoid';
 import { PlayerLocation } from '../types/CoveyTownSocket';
 import PlayerController from './PlayerController';
