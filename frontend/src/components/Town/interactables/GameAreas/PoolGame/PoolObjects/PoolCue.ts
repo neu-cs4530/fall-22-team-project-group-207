9693b18a8fd789f03a819e300344a693f6cb379e
bperritt <bradleyperritt0@gmail.com>
2022-11-29 15:20:25 -0500

    Fixed getters and setters

--- frontend/src/components/Town/interactables/GameAreas/PoolGame/PoolObjects/PoolCue.ts
+++ frontend/src/components/Town/interactables/GameAreas/PoolGame/PoolObjects/PoolCue.ts
@@ -3,21 +3,29 @@ import { Vector } from '../Vector';
 const CUE_MASS = 0.5386; // kg
 
 export default class PoolCue {
-  private _velocity: Vector;
+  private _velocity: Vector = { x: 0, y: 0, z: 0 };
 
-  private _position: Vector;
+  private _position: Vector = { x: 0, y: 0, z: 0 };
 
   constructor(velocity: Vector, position: Vector) {
-    this._velocity = velocity;
-    this._position = position;
+    Object.assign(this._velocity, velocity);
+    Object.assign(this._position, position);
   }
 
   get velocity() {
-    return this._velocity;
+    return { x: this._velocity.x, y: this._velocity.y, z: this._velocity.z };
+  }
+
+  set velocity(v: Vector) {
+    Object.assign(this._velocity, v);
   }
 
   get position() {
-    return this._position;
+    return { x: this._position.x, y: this._position.y, z: this._position.z };
+  }
+
+  set position(v: Vector) {
+    Object.assign(this._position, v);
   }
 
   get mass(): number {
